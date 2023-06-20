1fe16abb9fe7412bdf1970f24e21622442b4cfc5
Brian Quach <brianquach@berkeley.edu>
2023-06-19 19:09:52 -0700

    adding head section of landing page

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,7 +5,19 @@ import "./App.css";
 function App() {
     return (
         <div>
-            <h1> Cookbook.AI </h1>
+            <div className="head">
+                <div className="head1">
+                    <h1>Cookbook.AI</h1>
+                    <h3> — Your AI powered kitchen assistant. </h3>
+                </div>
+                <div className="button" id="cookingButton">
+                    <div id="dub-arrow"><img src="https://github.com/atloomer/atloomer.github.io/blob/master/img/iconmonstr-arrow-48-240.png?raw=true" alt="" /></div>
+                    <a href="#">Get cooking!</a>
+                </div>
+            </div>
+            <div className="howContainer">
+                <h1>How it works</h1>
+            </div>
         </div>
     )
 }
